1edf0baad74ab1ac475fa30abaf4f5dd11d36f92
Jan Cieślak <s19096@pjwstk.edu.pl>
2020-11-02 17:47:03 +0100

    Implement chain of responsibility calculator

---
+++ Assignment IV V/src/group_one/calculator/Calculator.kt
@@ -0,0 +1,139 @@
+package group_one.calculator
+
+import java.lang.IllegalStateException
+
+abstract class ExpressionHandler {
+    private var next: ExpressionHandler? = null
+
+    fun then(next: ExpressionHandler): ExpressionHandler {
+        this.next = next
+        return next
+    }
+
+    abstract fun handle(expression: String): Boolean
+
+    protected fun handleNext(expression: String): Boolean {
+        if (this.next == null) {
+            return true
+        }
+        return next!!.handle(expression)
+    }
+}
+
+abstract class BaseExpressionHandler : ExpressionHandler() {
+    protected abstract val sign: String
+
+    private fun handleExpression(expression: String): String {
+        val splitExpression = expression.split(" ")
+        val newExpression = StringBuilder()
+
+        if (splitExpression.size <= 2)
+            throw IllegalStateException("Wrong expressions")
+
+        var i = 0
+        while (i < splitExpression.size) {
+            if (i + 1 < splitExpression.size && splitExpression[i + 1] == sign) {
+                val result = calculate(splitExpression[i].toInt(), splitExpression[i + 2].toInt())
+                newExpression.append("$result ")
+                i += 2
+            }
+            else {
+                newExpression.append("${splitExpression[i]} ")
+            }
+
+            i++
+        }
+
+        return newExpression.toString()
+    }
+
+    override fun handle(expression: String): Boolean  {
+        var newExpression = handleExpression(expression)
+
+        while (sign in newExpression) {
+            newExpression = handleExpression(newExpression)
+        }
+
+        return handleNext(newExpression)
+    }
+
+    protected abstract fun calculate(left: Int, right: Int): Int
+}
+
+class MultiplyExpressionHandler : BaseExpressionHandler() {
+    override val sign: String
+        get() = "*"
+
+    override fun calculate(left: Int, right: Int): Int {
+        return left * right
+    }
+}
+
+class DivideExpressionHandler : BaseExpressionHandler() {
+    override val sign: String
+        get() = "/"
+
+    override fun calculate(left: Int, right: Int): Int {
+        return left / right
+    }
+}
+
+class AddExpressionHandler : BaseExpressionHandler() {
+    override val sign: String
+        get() = "+"
+
+    override fun calculate(left: Int, right: Int): Int {
+        return left + right
+    }
+}
+
+class MinusExpressionHandler: BaseExpressionHandler() {
+    override val sign: String
+        get() = "-"
+
+    override fun calculate(left: Int, right: Int): Int {
+        return left - right
+    }
+}
+
+class PrintExpression: ExpressionHandler() {
+    override fun handle(expression: String): Boolean {
+        println(expression)
+        return handleNext(expression)
+    }
+}
+
+fun main() {
+    val expression = "5 * 5 + 2 - 1 + 23 / 5 * 2 + 3"
+    val calculator = PrintExpression()
+
+    println("Normal calculator:")
+
+    calculator
+        .then(MultiplyExpressionHandler()) // multiply
+        .then(PrintExpression())
+        .then(DivideExpressionHandler()) // divide
+        .then(PrintExpression())
+        .then(AddExpressionHandler()) // add
+        .then(PrintExpression())
+        .then(MinusExpressionHandler()) // subtract
+        .then(PrintExpression())
+
+    calculator.handle(expression)
+
+    println("Strange calculator:")
+
+    // a calculator that add -> divide -> subtract -> multiply - because i can
+    val strangeCalculator = PrintExpression()
+    strangeCalculator
+        .then(AddExpressionHandler()) // add
+        .then(PrintExpression())
+        .then(DivideExpressionHandler()) // divide
+        .then(PrintExpression())
+        .then(MinusExpressionHandler()) // subtract
+        .then(PrintExpression())
+        .then(MultiplyExpressionHandler()) // multiply
+        .then(PrintExpression())
+
+    strangeCalculator.handle(expression)
+}
